012ad30ae448df499b88961e4548f1fc735c690f
jlai <result0924@gmail.com>
2019-06-06 18:35:39 +0800

    Add Quote Model

---
+++ RealtimeTrafficInfomation/RealtimeTrafficInfomation/Models/Quote.swift
@@ -0,0 +1,16 @@
+//
+//  Quote.swift
+//  RealtimeTrafficInfomation
+//
+//  Created by justin on 2019/6/6.
+//  Copyright © 2019 jlai. All rights reserved.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    let text: String
+    let author: String
+    let title: String
+    let date: Date
+}
