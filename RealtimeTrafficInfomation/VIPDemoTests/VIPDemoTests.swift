3382fea9bbe35feb3b4358f105972431ee39ff9c
jlai <result0924@gmail.com>
2019-06-18 17:31:22 +0800

    Add VIPDemoTests target

---
+++ RealtimeTrafficInfomation/VIPDemoTests/VIPDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  VIPDemoTests.swift
+//  VIPDemoTests
+//
+//  Created by justin on 2019/6/18.
+//  Copyright © 2019 jlai. All rights reserved.
+//
+
+import XCTest
+
+class VIPDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
